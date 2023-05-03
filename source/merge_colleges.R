2d0bcdc483629f24aefe0305944db0e8cac9f0b6
Samantha Gold <skeigold@gmail.com>
2023-05-03 10:36:08 -0400

    edits to assembling college mobility location data

--- source/merge_colleges.R
+++ source/merge_colleges.R
@@ -1,31 +1,35 @@
 
 # -------------------------------------------------------------------------
 ## Purpose: merge college scorecard data together 
-
 # -------------------------------------------------------------------------
-## Data file will be used in maps - points representing different types 
-## of colleges. 
+## produce a file that has the following columns opeid6 | mr_kq5_pq1 | longitude | latitude
+## distinct at the opeid6 level ()
 
 # Setup -------------------------------------------------------------------
 library(tidyverse)
 chetty_scorecard <- "/Users/sammygold/Documents/GitHub/PAM6950ExtendedAbstract/data/CollegeMobilityScorecard/clean/scorecard_cleaned.csv"
 addresses <- "/Users/sammygold/Documents/GitHub/PAM6950ExtendedAbstract/data/CollegeScoreCard/clean/CollegeScoreCard_cleaned.csv"
-outfile <- "/Users/sammygold/Documents/GitHub/PAM6950ExtendedAbstract/data/UScolleges.csv"
-
+outfile <- "/Users/sammygold/Documents/GitHub/PAM6950ExtendedAbstract/data/schoolmobility_locations.csv"
 # -------------------------------------------------------------------------
-
-
 # Merge -------------------------------------------------------------------
-read_csv(chetty_scorecard) %>%
+schools <- read_csv(chetty_scorecard) %>%
   inner_join(read_csv(addresses) %>% mutate(opeid6 = as.numeric(opeid6)), by = c("opeid" = "opeid6")) %>% 
   mutate(latitude = case_when(latitude == "NULL" ~ NA_character_, 
                               TRUE ~ latitude), 
          longitude = case_when(longitude == "NULL" ~ NA_character_, 
                                TRUE ~ longitude)) %>% 
-  group_by(super_opeid) %>% 
-  fill(latitude:longitude, .direction = "updown") %>% 
-  ungroup() %>% 
-  distinct(super_opeid, .keep_all = TRUE) 
+  filter(
+         !is.na(latitude), # dropping any schools with no lat/long 
+         name != "Colleges with insufficient data") %>% # dropping schools with insufficient data 
+  select(super_opeid, instnm, mr_kq5_pq1, latitude, longitude) %>% 
+  arrange(super_opeid) %>% 
+  distinct(instnm, .keep_all = TRUE) 
+
+write_csv(schools, outfile)
+  
+
+  
+
   
   
   
